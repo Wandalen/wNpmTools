6a24588edde42c90dc4ba24aebe44a63e2b23596
Wandalen <wandalen.me@gmail.com>
2019-10-23 18:16:09 +0300

    add files

---
+++ proto/dwtools/amid/l3/npm/IncludeMid.s
@@ -0,0 +1,20 @@
+( function _IncludeMid_s_( ) {
+
+'use strict';
+
+/**
+ * Collection of tools to use npm programmatically.
+  @module Tools/mid/NpmTools
+*/
+
+if( typeof module !== 'undefined' )
+{
+
+  let _ = require( '../../../Tools.s' );
+
+  require( './IncludeBase.s' );
+  require( './l1/Tools.s' );
+
+}
+
+})();
